0e98b0763957281a45ad2e9d842bd69c81bcf982
ThetNaingTun <67203393+thetnaingtunerp@users.noreply.github.com>
2026-05-14 09:02:43 +0630

    pip

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # AISpeech
 
-pip install moviepy speechrecognition pydub
+pip install speechrecognition pydub
+
+
+pip install openai-whisper
+
+pip install torch
+
+pip install moviepy==1.0.3
--- mysite/videoscript/models.py
+++ mysite/videoscript/models.py
@@ -7,17 +7,11 @@ class Video(models.Model):
     summary = models.TextField()
 
     video = models.FileField(upload_to='videos/')
+    transcript = models.TextField(blank=True, null=True)
 
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
-# Create your models here.
-class VideoUpload(models.Model):
-    video = models.FileField(upload_to='videos/')
-    audio = models.FileField(upload_to='audio/', blank=True, null=True)
-    transcript = models.TextField(blank=True, null=True)
-    uploaded_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"Video {self.id} - {self.uploaded_at}"
+
